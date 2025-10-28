2d6c53996af59c7473e5a1d8b068be7f982b68ab
Samantula <703441926@genpact.com>
2025-10-28 19:22:25 +0530

    first

---
+++ main.tf
@@ -0,0 +1,26 @@
+
+
+# 🧩 Retrieve credentials from AWS Secrets Managers
+data "aws_secretsmanager_secret_version" "example" {
+  secret_id = var.secret_name
+}
+
+locals {
+  secret_data = jsondecode(data.aws_secretsmanager_secret_version.example.secret_string)
+}
+
+# 🧠 Simple EC2 instance
+resource "aws_instance" "demo" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "TerraformDemo"
+  }
+}
+
+# 🧾 Output secrets (masked in console)
+output "retrieved_secret" {
+  value     = local.secret_data
+  sensitive = true
+}
